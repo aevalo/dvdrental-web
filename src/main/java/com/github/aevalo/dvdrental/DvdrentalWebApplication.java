e01e757b946dfbd01411a6fb87b623fc78f941b1
Arttu Valo <arttu.valo@gmail.com>
2021-10-02 22:35:59 +0300

    Initial project structure

---
+++ src/main/java/com/github/aevalo/dvdrental/DvdrentalWebApplication.java
@@ -0,0 +1,13 @@
+package com.github.aevalo.dvdrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdrentalWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvdrentalWebApplication.class, args);
+	}
+
+}
